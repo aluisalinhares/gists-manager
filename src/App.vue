3c1065f62ffc9868c8a5338757774f13c7e1e476
Ana Linhares <analinhares@anas-mbp.lan>
2024-02-15 15:43:31 +0000

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,19 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <button @click="login">Login with GitHub</button>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  methods: {
+    async login() {
+      // Redirect to GitHub authorization endpoint
+      window.location.href = `https://github.com/login/oauth/authorize?client_id=d9e20d499a3894452cdd&scope=user`;
+    }
   }
 }
 </script>
