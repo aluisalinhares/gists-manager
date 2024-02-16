21f8d80bd8d61d2bc0c0cad5be1600ba2eca14ec
Ana Linhares <analinhares@anas-mbp.lan>
2024-02-16 18:20:00 +0000

    update oauth handling

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,54 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
     <button @click="login">Login with GitHub</button>
-    <div>new</div>
   </div>
 </template>
 
 <script>
 import { useAuthStore } from '@/store';
+
 const clientId = process.env.VUE_APP_GITHUB_CLIENT_ID
 const clientSecret = process.env.VUE_APP_GITHUB_CLIENT_SECRET
+console.log("process.env: ", process.env)
+console.log("Client", clientId, clientSecret)
 
-console.log("here: ", process.env)
 export default {
   name: 'App',
+  mounted() {
+    console.log("mounted called")
+    if (window.location.search.includes('code')) {
+      console.log("includes code")
+      this.handleCallback();
+    }
+  },
   methods: {
     async login() {
-      console.log("login called")
       // Redirect to GitHub authorization endpoint
       window.location.href = `https://github.com/login/oauth/authorize?client_id=${clientId}&scope=user`;
-
     },
     async handleCallback() {
       console.log("handleCallback called")
       const code = new URLSearchParams(window.location.search).get('code');
       console.log("code", code);
 
-      const response = await fetch(`https://github.com/login/oauth/access_token?client_id=${clientId}&code=${code}`, {
+      const fetchOptions = {
         method: 'POST',
         headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json',
+          'Authorization': personalAccessToken ? `token ${personalAccessToken}` : null  // Use personal access token if available
+        },
+        body: JSON.stringify({
+          client_id: clientId,
+          client_secret: clientSecret,
+          code: code
+        })
+      };
+
+      const response = await fetch(`https://github.com/login/oauth/access_token`, {
+        method: 'POST',
+        headers: {
+
           'Content-Type': 'application/json',
           'Accept': 'application/json'
         },
@@ -41,20 +61,14 @@ export default {
 
       const data = await response.json();
       const accessToken = data.access_token;
+      console.log("accessToken", accessToken);
       // Save access token to local storage
       localStorage.setItem('accessToken', accessToken);
 
       // Redirect to home page or wherever you need to go after authentication
       window.location.href = '/test';
       useAuthStore().setAccessToken(accessToken); // Save access token to store
-    },
-    mounted() {
-      if (window.location.search.includes('code')) {
-        console.log("includes code")
-        this.handleCallback();
-      }
     }
-
   }
 }
 </script>
